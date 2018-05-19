bcda8362f9fa71b9d4a6a106d32076d3c57ce3a7
{bobchin} <kinjo.bobchin@gmail.com>
2018-05-20 03:43:53 +0900

    pug、sassに拡張子を変換して中身も書き換え

--- src/app/reference-list/reference-list.component.ts
+++ src/app/reference-list/reference-list.component.ts
@@ -2,14 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reference-list',
-  templateUrl: './reference-list.component.html',
-  styleUrls: ['./reference-list.component.css']
+  templateUrl: './reference-list.component.pug',
+  styleUrls: ['./reference-list.component.sass']
 })
 export class ReferenceListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
