ae56ff0ec596a293e669349395d0ac79ea9b8357
{bobchin} <kinjo.bobchin@gmail.com>
2018-05-20 03:34:57 +0900

    ng g component reference-list でよく参考にするリンクをコンポーネント化する

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReferenceListComponent } from './reference-list/reference-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReferenceListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/reference-list/reference-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reference-list',
+  templateUrl: './reference-list.component.html',
+  styleUrls: ['./reference-list.component.css']
+})
+export class ReferenceListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
